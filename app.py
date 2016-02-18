2a9f735c794439fcb4e03f54b584e5b023f1b083
christaborium <dxdstudio@gmail.com>
2016-02-17 20:47:03 -0800

    Add placeholder app content

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+"""This is your typical app, demonstrating usage."""
+
 from flask import Flask
 from charts_builder import charts
 
@@ -6,6 +8,12 @@ app.config['SECRET_KEY'] = 'NOTSECURELOL'
 app.debug = True
 
 app.register_blueprint(charts)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    """Sample index."""
+    return '<a href="/charts">Visit the charts blueprint.</a>'
 
 
 if __name__ == '__main__':
