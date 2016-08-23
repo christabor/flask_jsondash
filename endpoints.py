f0d210a535b8d0576b4f3da175b06a9061d2389a
christaborium <dxdstudio@gmail.com>
2016-08-23 15:44:52 -0700

    Add stacked bar example and config.

--- endpoints.py
+++ endpoints.py
@@ -68,6 +68,30 @@ def combination():
 
 
 @cross_origin()
+@app.route('/stacked-bar')
+def stackedbar():
+    """Fake endpoint."""
+    return json.dumps({
+        'data': {
+            'columns': [
+                ['data1', -30, 200, 200, 400, -150, 250],
+                ['data2', 130, 100, -100, 200, -150, 50],
+                ['data3', -230, 200, 200, -300, 250, 250]
+            ],
+            'type': 'bar',
+            'groups': [
+                ['data1', 'data2']
+            ]
+        },
+        'grid': {
+            'y': {
+                'lines': [{'value': 0}]
+            }
+        }
+    })
+
+
+@cross_origin()
 @app.route('/timeline')
 def timeline():
     """Fake endpoint."""
