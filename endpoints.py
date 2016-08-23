3eee82b2b281fec90315f93de27ced47a84f7f44
christaborium <dxdstudio@gmail.com>
2016-08-23 11:14:25 -0700

    Add c3 combination example endpoint.

--- endpoints.py
+++ endpoints.py
@@ -42,6 +42,32 @@ def rr_list(max_range=10):
 
 
 @cross_origin()
+@app.route('/combination/')
+def combination():
+    """Fake endpoint."""
+    data = {
+        'columns': [
+            ['data1', 30, 20, 50, 40, 60, 50],
+            ['data2', 200, 130, 90, 240, 130, 220],
+            ['data3', 300, 200, 160, 400, 250, 250],
+            ['data4', 200, 130, 90, 240, 130, 220],
+            ['data5', 130, 120, 150, 140, 160, 150],
+            ['data6', 90, 70, 20, 50, 60, 120],
+        ],
+        'type': 'bar',
+        'types': {
+            'data3': 'spline',
+            'data4': 'line',
+            'data6': 'area',
+        },
+        'groups': [
+            ['data1', 'data2'],
+        ]
+    }
+    return json.dumps(dict(data=data))
+
+
+@cross_origin()
 @app.route('/timeline/')
 def timeline():
     """Fake endpoint."""
