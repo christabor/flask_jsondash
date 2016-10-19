87074bed5a9627566417278d1c3564eccf1015ca
christaborium <dxdstudio@gmail.com>
2016-10-19 11:22:15 -0700

    Refactor auth checks:
    The old auth check logic was awkward and verbose. The boolean has been inverted
    so it intuitively makes more sense, and the auth enabled and auth check functions
    have been condensed into one place.
    
    Also, when creating a new view, redirect will now go to the newly created page,
    not the dashboard.

--- flask_jsondash/charts_builder.py
+++ flask_jsondash/charts_builder.py
@@ -53,29 +53,23 @@ default_config = dict(
 )
 
 
-def auth_enabled(authtype=None):
+def auth(**kwargs):
     """Check if general auth functions have been specified.
 
     Checks for either a global auth (if authtype is None), or
     an action specific auth (specified by authtype).
     """
+    authtype = kwargs.pop('authtype')
     if 'JSONDASH' not in current_app.config:
-        return False
+        return True
     if 'auth' not in current_app.config['JSONDASH']:
-        return False
+        return True
     auth_conf = current_app.config.get('JSONDASH').get('auth')
-    if authtype is not None:
-        return authtype in auth_conf
-    return True
-
-
-def auth_check(authtype, **kwargs):
-    """Check if a user specified validator works for access to a given type.
-
-    This does not check if a specific auth function was defined, and assumes
-    that is done elsewhere.
-    """
-    return current_app.config['JSONDASH']['auth'][authtype](**kwargs)
+    if all([authtype is not None, authtype in auth_conf]):
+        # Only perform the user-supplied check
+        # if the authtype is actually enabled.
+        return current_app.config['JSONDASH']['auth'][authtype](**kwargs)
+    return False
 
 
 def metadata(key=None):
@@ -193,10 +187,9 @@ def dashboard():
 @charts.route('/charts/<id>', methods=['GET'])
 def view(id):
     """Load a json view config from the DB."""
-    if auth_enabled(authtype='view'):
-        if not auth_check('view', view_id=id):
-            flash('You do not have access to view this dashboard.', 'error')
-            return redirect(url_for('jsondash.dashboard'))
+    if not auth(authtype='view'):
+        flash('You do not have access to view this dashboard.', 'error')
+        return redirect(url_for('jsondash.dashboard'))
     viewjson = adapter.read(c_id=id)
     if not viewjson:
         flash('Could not find view: {}'.format(id), 'error')
@@ -214,10 +207,9 @@ def view(id):
 def delete(c_id):
     """Delete a json dashboard config."""
     dash_url = url_for('jsondash.dashboard')
-    if auth_enabled(authtype='delete'):
-        if not auth_check('delete'):
-            flash('You do not have access to delete dashboards.', 'error')
-            return redirect(dash_url)
+    if not auth(authtype='delete'):
+        flash('You do not have access to delete dashboards.', 'error')
+        return redirect(dash_url)
     adapter.delete(c_id)
     flash('Deleted dashboard {}'.format(c_id))
     return redirect(dash_url)
@@ -226,10 +218,9 @@ def delete(c_id):
 @charts.route('/charts/update', methods=['POST'])
 def update():
     """Normalize the form POST and setup the json view config object."""
-    if auth_enabled(authtype='update'):
-        if not auth_check('update'):
-            flash('You do not have access to update dashboards.', 'error')
-            return redirect(url_for('jsondash.dashboard'))
+    if not auth(authtype='update'):
+        flash('You do not have access to update dashboards.', 'error')
+        return redirect(url_for('jsondash.dashboard'))
     data = request.form
     c_id = data['id']
     view_url = url_for('jsondash.view', id=c_id)
@@ -259,31 +250,30 @@ def update():
 @charts.route('/charts/create', methods=['POST'])
 def create():
     """Normalize the form POST and setup the json view config object."""
-    if auth_enabled(authtype='create'):
-        if not auth_check('create'):
-            flash('You do not have access to create dashboards.', 'error')
-            return redirect(url_for('jsondash.dashboard'))
+    if not auth(authtype='create'):
+        flash('You do not have access to create dashboards.', 'error')
+        return redirect(url_for('jsondash.dashboard'))
     data = request.form
+    new_id = str(uuid.uuid1())
     d = dict(
         name=data['name'],
         modules=adapter._format_modules(data),
         date=dt.now(),
-        id=str(uuid.uuid1()),
+        id=new_id,
     )
     d.update(**metadata())
     # Add to DB
     adapter.create(data=d)
     flash('Created new view "{}"'.format(data['name']))
-    return redirect(url_for('jsondash.dashboard'))
+    return redirect(url_for('jsondash.view', id=new_id))
 
 
 @charts.route('/charts/clone/<c_id>', methods=['POST'])
 def clone(c_id):
     """Clone a json view config from the DB."""
-    if auth_enabled(authtype='clone'):
-        if not auth_check('clone'):
-            flash('You do not have access to clone dashboards.', 'error')
-            return redirect(url_for('jsondash.dashboard'))
+    if not auth(authtype='clone'):
+        flash('You do not have access to clone dashboards.', 'error')
+        return redirect(url_for('jsondash.dashboard'))
     viewjson = adapter.read(c_id=c_id)
     if not viewjson:
         flash('Could not find view: {}'.format(id), 'error')
