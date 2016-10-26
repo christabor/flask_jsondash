6316f552f68d561d29275a4ff3bc6fff2b12eb98
christaborium <dxdstudio@gmail.com>
2016-10-25 22:13:11 -0700

    Fix version.

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ def readme():
 
 setup(
     name='flask_jsondash',
-    version='4.10.2',
+    version='4.0.0',
     description=('Easily configurable, chart dashboards from any '
                  'arbitrary API endpoint. JSON config only. Ready to go.'),
     long_description=readme(),
