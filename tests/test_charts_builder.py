e542dadb015de79f125de5c6a633278f499168ca
christaborium <dxdstudio@gmail.com>
2016-11-08 21:54:45 -0800

    Add more tests.

--- tests/test_charts_builder.py
+++ tests/test_charts_builder.py
@@ -12,12 +12,35 @@ app.debug = True
 app.register_blueprint(charts_builder.charts)
 
 
+def _username():
+    return 'Username'
+
+
+def _authtest():
+    return False
+
+
 @pytest.fixture
 def client(request):
     app.config.update(
         JSONDASH_GLOBALDASH=False,
         JSONDASH_FILTERUSERS=False,
         JSONDASH_GLOBAL_USER='global-test',
+    )
+    app.config['JSONDASH'] = dict(
+        metadata=dict(
+            created_by=_username,
+            username=_username,
+        ),
+        static=dict(
+            js_path='js/vendor/',
+            css_path='css/vendor/',
+        ),
+        auth=dict(
+            edit_global=_authtest,
+            clone=_authtest,
+            delete=_authtest,
+        )
     )
     return app.test_client()
 
@@ -34,3 +57,9 @@ def test_setting(client):
         assert _get('JSONDASH_GLOBALDASH') == False
         assert _get('JSONDASH_FILTERUSERS') == False
         assert _get('JSONDASH_GLOBAL_USER') == 'global-test'
+
+
+def test_metadata(client):
+    with app.app_context():
+        assert charts_builder.metadata(key='created_by') == 'Username'
+        assert charts_builder.metadata(key='username') == 'Username'
