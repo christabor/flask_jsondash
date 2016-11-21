d4442364308175ccd2d7f97bbecc30f164729159
christaborium <dxdstudio@gmail.com>
2016-11-21 13:27:20 -0800

    Clean up and simplify paginator tests, add more.

--- tests/test_paginator.py
+++ tests/test_paginator.py
@@ -19,19 +19,23 @@ def make_setting(num=30):
     return setting
 
 
-def test_paginator_default(monkeypatch, client):
+def check_values(paginator, limit, perpage, currpage, skip, numpages, count):
+    assert isinstance(paginator, charts_builder.Paginator)
+    assert paginator.limit == limit
+    assert paginator.per_page == perpage
+    assert paginator.curr_page == currpage
+    assert paginator.skip == skip
+    assert paginator.num_pages == numpages
+    assert paginator.count == count
+
+
+def test_paginator_default_usage(monkeypatch, client):
     app, test = client
     with app.app_context():
         monkeypatch.setattr(charts_builder, 'setting', make_setting(30))
         monkeypatch.setattr(charts_builder.adapter, 'count', make_count(1000))
-        paginator = charts_builder.paginator(0)
-        assert isinstance(paginator, charts_builder.Paginator)
-        assert paginator.limit == 30
-        assert paginator.per_page == 30
-        assert paginator.curr_page == 0
-        assert paginator.skip == 0
-        assert paginator.num_pages == range(1, 35)
-        assert paginator.count == 1000
+        paginator = charts_builder.paginator(page=0)
+        check_values(paginator, 30, 30, 0, 0, range(1, 35), 1000)
 
 
 def test_paginator_norecords(monkeypatch, client):
@@ -39,11 +43,24 @@ def test_paginator_norecords(monkeypatch, client):
     with app.app_context():
         monkeypatch.setattr(charts_builder, 'setting', make_setting(30))
         monkeypatch.setattr(charts_builder.adapter, 'count', make_count(0))
-        paginator = charts_builder.paginator(0)
-        assert isinstance(paginator, charts_builder.Paginator)
-        assert paginator.limit == 30
-        assert paginator.per_page == 30
-        assert paginator.curr_page == 0
-        assert paginator.skip == 0
-        assert paginator.num_pages == []
-        assert paginator.count == 0
+        paginator = charts_builder.paginator(page=0)
+        check_values(paginator, 30, 30, 0, 0, [], 0)
+
+
+def test_paginator_default_fallback_data(monkeypatch, client):
+    app, test = client
+    with app.app_context():
+        monkeypatch.setattr(charts_builder, 'setting', make_setting(0))
+        monkeypatch.setattr(charts_builder.adapter, 'count', make_count(0))
+        paginator = charts_builder.paginator(page=None, per_page=1, count=None)
+        check_values(paginator, 2, 2, 0, 0, [], 0)
+
+
+def test_paginator_bad_kwargs(monkeypatch, client):
+    app, test = client
+    with app.app_context():
+        monkeypatch.setattr(charts_builder, 'setting', make_setting(0))
+        monkeypatch.setattr(charts_builder.adapter, 'count', make_count(0))
+        paginator = charts_builder.paginator(
+            page=None, per_page=None, count=None)
+        check_values(paginator, 30, 30, 0, 0, [], 0)
