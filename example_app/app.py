39bc88808d9286f7d6a74120b8d8bade9888e41c
christaborium <dxdstudio@gmail.com>
2016-11-30 22:42:54 -0800

    Enable global by default in example.

--- example_app/app.py
+++ example_app/app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'NOTSECURELOL'
 app.config.update(
     JSONDASH_FILTERUSERS=False,
-    JSONDASH_GLOBALDASH=False,
+    JSONDASH_GLOBALDASH=True,
     JSONDASH_GLOBAL_USER='global',
 )
 app.debug = True
