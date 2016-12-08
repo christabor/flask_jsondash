ce5f6d993f1473aa3cb2caa2869e22fa53c46b22
christaborium <dxdstudio@gmail.com>
2016-12-07 20:36:30 -0800

    Add some more tests.

--- tests/test_charts_builder.py
+++ tests/test_charts_builder.py
@@ -1,6 +1,7 @@
 import json
 import os
 from datetime import datetime as dt
+from random import shuffle
 
 from flask import (
     Flask,
@@ -165,6 +166,32 @@ def test_order_sort_invalid_key():
 def test_order_sort_valid_key():
     item = dict(order=1)
     assert charts_builder.order_sort(item) == 1
+
+
+@pytest.mark.utils
+def test_order_shuffled_sort_multiple_valid_key():
+    orders = range(0, 10)
+    shuffle(orders)
+    modules = [dict(order=i, foo='bar') for i in orders]
+    res = sorted(modules, key=charts_builder.order_sort)
+    for i in range(0, 10):
+        assert res[i]['order'] == i
+
+
+@pytest.mark.utils
+def test_order_shuffled_sort_multiple_valid_key_one_invalid_key():
+    orders = range(0, 10)
+    shuffle(orders)
+    modules = [dict(order=i, foo='bar') for i in orders]
+    # Add one w/o order key.
+    modules.append(dict(foo='bar'))
+    res = sorted(modules, key=charts_builder.order_sort)
+    # The invalid key goes first.
+    assert 'order' not in res[0]
+    # The remaining will be offset since the first one has no key.
+    for i in range(0, 10):
+        if 'order' in res[i]:
+            assert res[i]['order'] == i - 1
 
 
 def test_app_redirects(ctx, client):
