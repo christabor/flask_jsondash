ebbd13f2d907c3ab3727f4aa78ed69846309a68d
christaborium <dxdstudio@gmail.com>
2016-12-07 22:35:59 -0800

    Add some more tests.

--- tests/test_paginator.py
+++ tests/test_paginator.py
@@ -4,7 +4,16 @@ from flask import (
     url_for,
 )
 
+from pyquery import PyQuery as pq
+
 from flask_jsondash import charts_builder
+
+from conftest import (
+    URL_BASE,
+    auth_ok,
+    read,
+    update,
+)
 
 
 def make_count(num=1000):
@@ -60,3 +69,24 @@ def test_paginator_bad_kwargs(monkeypatch, client):
     paginator = charts_builder.paginator(
         page=None, per_page=None, count=None)
     check_values(paginator, 30, 30, 0, 0, [], 0)
+
+
+def test_create_dashboards_check_paginator_html(monkeypatch, ctx, client):
+    app, test = client
+    monkeypatch.setattr(charts_builder, 'auth', auth_ok)
+    for i in range(100):
+        data = dict(name=i, modules=[])
+        res = test.post(url_for('jsondash.create'), data=data)
+    res = test.get(url_for('jsondash.dashboard'))
+    dom = pq(res.data)
+    assert len(read()) == 100
+    assert dom.find('.paginator-status').text() == 'Showing 0-25 of 100 results'
+    res = test.get(url_for('jsondash.dashboard') + '?page=2')
+    dom = pq(res.data)
+    assert dom.find('.paginator-status').text() == 'Showing 25-50 of 100 results'
+    res = test.get(url_for('jsondash.dashboard') + '?page=3')
+    dom = pq(res.data)
+    assert dom.find('.paginator-status').text() == 'Showing 50-75 of 100 results'
+    res = test.get(url_for('jsondash.dashboard') + '?page=4')
+    dom = pq(res.data)
+    assert dom.find('.paginator-status').text() == 'Showing 75-100 of 100 results'
